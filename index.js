8a9d9dea9f20ec165b41b3134f5f00bd2562a67b
pooja <pooja@gamil.com>
2024-08-30 14:12:40 +0530

    index update

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 const express = require("express")
-const mongoose = require("mongoose")
 const cors = require("cors")
-const { httpServer, app } = require("./socket/socket")
+const mongoose = require("mongoose")
+const { app, httpServer } = require("./socket/socket")
 require("dotenv").config()
-// const path = require("path")
-
+const path = require("path")
 // const app = express()
+// console.log(__dirname)
+
+
+
+// Setp1 middleware
 app.use(express.json())
 app.use(express.static("dist"))
 app.use(cors({ origin: true, credentials: true }))
-app.use("/api/blog", require("./routes/blog.routes"))
 
+// step2 routes
+app.use("/api/blog", require("./routes/blog.routes"))
+// tep 3 404 routes
 app.use("*", (req, res) => {
-    // res.sendFile(path.join(__dirname, "dist", "index.html"))
-    res.status(404).json({ message: "Resource not Found" })
+
+    // dirname always return absalute path
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "resource not found 404" })
 })
+// tep 4 error handler
 app.use((err, req, res, next) => {
     console.log(err);
 
     res.status(500).json({ message: "SERVER ERROR", error: err.message })
 })
-
+// step 5 connection
 mongoose.connect(process.env.MONGO_URL)
 
 mongoose.connection.once("open", () => {
-    console.log("MONGO CONNECTED")
+    console.log("MONGO CONNECTED");
     httpServer.listen(process.env.PORT, console.log("SERVER RUNNING"))
 
 })
