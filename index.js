4b88293f43f1fe682d37f7a25257aace4a5b7f29
pooja <pooja@gamil.com>
2024-08-30 14:05:45 +0530

    indez

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 // const app = express()
 app.use(express.json())
 app.use(express.static("dist"))
-
+app.use(cors({ origin: true, credentials: true }))
 app.use("/api/blog", require("./routes/blog.routes"))
 
 app.use("*", (req, res) => {
